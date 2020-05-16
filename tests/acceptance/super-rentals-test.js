648b644e1310903fa3a453180717698996e3ab8a
Jonny Heemstra <jonathan.heemstra@gmail.com>
2020-05-16 14:13:58 -0700

    Part 1 - automated testing

---
+++ tests/acceptance/super-rentals-test.js
@@ -0,0 +1,37 @@
+import { setupApplicationTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { click, currentURL, visit } from '@ember/test-helpers';
+
+module('Acceptance | super rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /super-rentals', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/');
+    assert.dom('h2').hasText('Welcome to Super Rentals!');
+    assert.dom('.jumbo a.button').hasText('About Us');
+
+    await click('.jumbo a.button');
+    assert.equal(currentURL(), '/about');
+  });
+
+  test('visiting /about', async function(assert) {
+    await visit('/about');
+    assert.equal(currentURL(), '/about');
+    assert.dom('h2').hasText('About Super Rentals');
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+
+    await click('.jumbo a.button');
+    assert.equal(currentURL(), '/getting-in-touch');
+  });
+
+  test('visiting /getting-in-touch', async function(assert) {
+    await visit('/getting-in-touch');
+    assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('h2').hasText('Contact Us');
+    assert.dom('a.button').hasText('About Us');
+
+    await click('.jumbo a.button');
+    assert.equal(currentURL(), '/about');
+  });
+});
